036d6ba89f64a60dd41b5c1d6d2e3358d2e5fad3
erlmatt <96199777+erlmatt@users.noreply.github.com>
2025-12-27 22:13:49 +0100

    added models for implementation of adding a new databade centric approach to adding p systems

--- app/models.py
+++ app/models.py
@@ -1,5 +1,41 @@
 from pydantic import BaseModel
 from typing import Optional, List
+from sqlalchemy import *
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(String, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    pv_sytems = relationship("PVSystem", back_population="owner")
+
+class PVSystem(Base):
+    __tablename__ = "pv_sytems"
+    id = Column(Integer, primary_key=True, index = True)
+    user_id = Column(String, ForeignKey("user.id"))
+    name = Column(String)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    kwp = Column(Float)
+    tilt = Column(Float)
+    azimuth = Column(Float)
+    owner = relationship("User", back_populates="pv_sytems")
+
+class PVSystemBase():
+    name: str
+    latitude: float
+    longitude: float
+    kwp: float
+    tilt: float
+    azimuth: float
+
+class PVSystemCreate(PVSystemBase):
+    pass
+
+class PVSystemRead(PVSystemBase):
+    id: int 
+    user_id: str
 
 class Device(BaseModel):
     id: str
